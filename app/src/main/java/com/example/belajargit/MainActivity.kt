86a459066c74008bace039555464db2a90e75c5a
sukma-prabiansyah <sukmaprabiansyah9@gmail.com>
2023-09-07 16:41:58 +0700

    Menambahkan button dan fitur toast

--- app/src/main/java/com/example/belajargit/MainActivity.kt
+++ app/src/main/java/com/example/belajargit/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.belajargit
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button: Button = findViewById(R.id.button)
+        button.setOnClickListener {
+            Toast.makeText(this, "Hello, I am smart App!", Toast.LENGTH_SHORT).show()
+        }
     }
 }
