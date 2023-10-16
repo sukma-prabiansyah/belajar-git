871aca277816cb4f74535d4c9af52dbf956e710e
sukma-prabiansyah <sukmaprabiansyah9@gmail.com>
2023-10-16 10:37:20 +0700

    Menambahkan metode input age dan message string berdasarkan umur

--- app/src/main/java/com/example/belajargit/MainActivity.kt
+++ app/src/main/java/com/example/belajargit/MainActivity.kt
@@ -12,15 +12,27 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val etName: EditText = findViewById(R.id.et_name)
+        val etAge: EditText = findViewById(R.id.et_age)
         val button: Button = findViewById(R.id.button)
 
         val name = etName.text.toString()
+        val age = etAge.text.toString()
+
         button.setOnClickListener {
             when {
                 name.isEmpty() -> etName.error = "Put your name first"
+                age.isEmpty() -> etAge.error = "Put your age first"
                 else -> {
                     etName.text.clear()
-                    Toast.makeText(this, "Hello, I am smart App!", Toast.LENGTH_SHORT).show()
+                    etAge.text.clear()
+                    val ages = when (age.toInt()) {
+                        in 0..20 -> "you are so great younger"
+                        in 21..40 -> "you are adult"
+                        in 41..60 -> "you are older"
+                        else -> "keep your life pattern"
+                    }
+                    val message = "Your name $name, $ages"
+                    Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
                 }
             }
         }
