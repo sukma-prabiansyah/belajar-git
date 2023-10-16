0e8ca13262e648dd511df012355667704514ef9b
sukma-prabiansyah <sukmaprabiansyah9@gmail.com>
2023-10-16 10:24:49 +0700

    Membuat metode input nama

--- app/src/main/java/com/example/belajargit/MainActivity.kt
+++ app/src/main/java/com/example/belajargit/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.belajargit
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
@@ -10,9 +11,18 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val etName: EditText = findViewById(R.id.et_name)
         val button: Button = findViewById(R.id.button)
+
+        val name = etName.text.toString()
         button.setOnClickListener {
-            Toast.makeText(this, "Hello, I am smart App!", Toast.LENGTH_SHORT).show()
+            when {
+                name.isEmpty() -> etName.error = "Put your name first"
+                else -> {
+                    etName.text.clear()
+                    Toast.makeText(this, "Hello, I am smart App!", Toast.LENGTH_SHORT).show()
+                }
+            }
         }
     }
 }
